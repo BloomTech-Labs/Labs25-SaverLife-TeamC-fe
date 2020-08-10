f7d0ba0399684ca459acf8b30318a0b585af2cfe
Victoria  Topham <topham.victoria@gmail.com>
2020-08-10 14:02:33 -0700

    chore: link routing on home page for testing purposes

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -13,13 +13,25 @@ function RenderHomePage(props) {
           approach components.
         </p>
         <p>
-          <Link to="/profile-list">Profiles Example</Link>
+          <Link to="/signup">Signup component</Link>
         </p>
         <p>
-          <Link to="/example-list">Example List of Items</Link>
+          <Link to="/categories">categories component</Link>
         </p>
         <p>
-          <Link to="/datavis">Data Visualizations Example</Link>
+          <Link to="/logout">logout component</Link>
+        </p>
+        <p>
+          <Link to="/budget">budget component</Link>
+        </p>
+        <p>
+          <Link to="/profile">profile component</Link>
+        </p>
+        <p>
+          <Link to="/progress">progress component</Link>
+        </p>
+        <p>
+          <Link to="/transactions">transactions component</Link>
         </p>
         <p>
           <Button type="primary" onClick={() => authService.logout()}>
