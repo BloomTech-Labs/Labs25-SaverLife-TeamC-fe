4034eb6b254a8aa739bec5100c2e03f8d5ce1ba4
Victoria  Topham <topham.victoria@gmail.com>
2020-08-10 13:39:23 -0700

    chore: created components for categorized spending.

---
+++ src/components/CategorizedSpending/CategorizedSpending.js
@@ -0,0 +1,13 @@
+//Shows spending by category, lets the user look historically TODO: discuss if this should be represented against a budget
+
+import React from 'react';
+
+const CategorizedSpending = () => {
+  return (
+    <>
+      <h1>This is the CategorizedSpending component!</h1>
+    </>
+  );
+};
+
+export default CategorizedSpending;
