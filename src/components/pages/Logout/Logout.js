ca52d04abb11b5c2b5488c5272044b93124794f9
Victoria  Topham <topham.victoria@gmail.com>
2020-08-10 13:49:05 -0700

    chore: created components for the logout page.

---
+++ src/components/pages/Logout/Logout.js
@@ -0,0 +1,13 @@
+//allow the user to log out
+
+import React from 'react';
+
+const Logout = () => {
+  return (
+    <>
+      <h1>This is the Logout component!</h1>
+    </>
+  );
+};
+
+export default Logout;
