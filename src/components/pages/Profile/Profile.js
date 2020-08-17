d231a136bcaa5c6da7b4b63b85ac990724965624
Chelsea Wetzel <chelswetz@gmail.com>
2020-08-17 14:01:57 -0400

    style: preview boxes and footer

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -2,7 +2,34 @@
 import React, { useState } from 'react';
 import AppHeader from '../../common/AppHeader';
 import AppMenu from '../../common/AppMenu';
-import './profile.css';
+import styled from 'styled-components';
+
+const Body = styled.div`
+  width: 100%;
+  height: 800px;
+  background-color: #ecb7db;
+`;
+
+const PreviewSection = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const PreviewBox = styled.div`
+  font-size: 2rem;
+  width: 12rem;
+  height: 12rem;
+  padding: 0 5%;
+  border: 0.02rem solid grey;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.14), 0 2px 3px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+`;
+
+const Footer = styled.div`
+  /* going to make a sticky footer here */
+`;
 
 const Profile = () => {
   const [image, setImage] = useState('');
@@ -33,22 +60,46 @@ const Profile = () => {
       <AppHeader />
       <div className="content-container">
         <AppMenu />
-      </div>
-      <div>
-        <input
-          type="file"
-          name="file"
-          placeholder="Upload an Image"
-          onChange={uploadImage}
-        />
-        {loading ? (
-          <h3>Loading...</h3>
-        ) : (
-          <img
-            src={image}
-            style={{ height: '17rem', borderRadius: '50%', margin: '2%' }}
+        <div>
+          {' '}
+          {/* might switch this div to the Body styled component in the future */}
+          <input
+            type="file"
+            name="file"
+            placeholder="Upload an Image"
+            onChange={uploadImage}
           />
-        )}
+          {loading ? (
+            <h3>Loading...</h3>
+          ) : (
+            <img
+              src={image}
+              style={{ height: '17rem', borderRadius: '50%', margin: '2%' }}
+            />
+          )}
+          <hr className="solid"></hr>
+          <PreviewSection>
+            {/* I will be adding more accuracte previews */}
+            <PreviewBox>
+              <p>Your Savings Goal</p>
+            </PreviewBox>
+            <PreviewBox>
+              <p>Your Progress</p>
+            </PreviewBox>
+          </PreviewSection>
+          <PreviewSection>
+            <PreviewBox>
+              <p>Your Budget</p>
+            </PreviewBox>
+            <PreviewBox>
+              <p>Recent Transactions</p>
+            </PreviewBox>
+          </PreviewSection>
+        </div>
+        <Footer className="footer">
+          <h5>Want to learn how you can budget better?</h5>
+          <a href="#">Take our Quiz!</a>
+        </Footer>
       </div>
     </div>
   );
