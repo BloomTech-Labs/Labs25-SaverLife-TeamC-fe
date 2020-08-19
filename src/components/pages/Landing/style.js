5a0c24504ab8bf59c83aba21fb3eff059090a723
Christina Harris <christinathedesigner@outlook.com>
2020-08-19 13:44:42 -0700

    chore: completed responsive styling

--- src/components/pages/Landing/style.js
+++ src/components/pages/Landing/style.js
@@ -15,6 +15,20 @@ import { Wiggle } from './keyframes';
 export const MainContainerWrapper = Styled.div`
 display:flex;
 flex-direction:column;
+  @media (min-device-width: 1600px) 
+  and (max-device-width: 2000px) {
+    width:50%;
+    border: 1px solid lightslategray;
+    padding: 1%;
+    margin: 0 auto;
+  }
+    @media (min-device-width: 2500px) 
+  and (max-device-width: 3000px) {
+    width:30%;
+    border: 1px solid lightslategray;
+    padding: 1%;
+    margin: 0 auto;
+  }
 `;
 
 export const WrapperRow = Styled.div`
@@ -46,13 +60,20 @@ display: flex;
 flex-direction: Column;
 flex-wrap: nowrap;
 width: 100%;
-margin-top: 5%;
+margin: 12% 0;
 padding: 5% 3%;
 justify-content: space-between;
 text-align: justify;
 text-justify: inter-word;
 align-items: center;
-background: linear-gradient(19deg, rgba(236,183,219,1) 0%, rgba(145,194,222,1) 68%);
+border: 1px dashed #00a6af;
+border-radius: 25px;
+box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.14), 0 2px 3px rgba(0, 0, 0, 0.2);
+background: rgb(227,184,219);
+background: linear-gradient(240deg, rgba(227,184,219,1) 0%, rgba(255,255,255,1) 100%);
+  @media (min-width: 600px) {
+    border: none;
+      }
 `;
 
 export const WrapperColumn = Styled.div`
@@ -73,10 +94,15 @@ width: 80%;
 flex-direction:column;
 justify-content:center;
 align-items:center;
+  @media (min-width: 600px) {
+    margin: 10% 0;
+    width: 50%;
+      }
 `;
 
 export const TestimonialDiv = Styled.div`
 display: flex;
+width: 100%;
 flex-direction: Column;
 flex-wrap: nowrap;
 justify-content: center;
@@ -85,6 +111,10 @@ border: .5 solid lightgray;
 box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.14), 0 2px 3px rgba(0, 0, 0, 0.2);
 padding: 5%;
 margin: 8% 0;
+background:linear-gradient(83deg, rgba(145,194,222,1) 0%, rgba(255,255,255,1) 54%, rgba(145,194,222,1) 100%);
+  @media (min-width: 600px) {
+    width: 80%;
+      }
 `;
 
 export const Footer = Styled.div`
@@ -93,6 +123,12 @@ background: Black;
 color: white;
 justify-content:center;
 padding: 3%;
+@media (min-width: 800px) {
+    font-size: 1.4rem;
+    a {
+      font-size: 1.4rem;
+    }
+      }
 
 `;
 
@@ -100,32 +136,54 @@ padding: 3%;
 
 export const Header = Styled.h2`
 color:white;
-font-size: 2rem;
+font-size: 1.8rem;
 font-weight: 600;
+  @media (min-width: 600px) {
+    font-size: 2rem;
+      }
+  @media (min-width: 800px) {
+    font-size: 3rem;
+      }
 `;
 
 export const HeaderPurple = Styled.h2`
 color:#c01089;
 font-size: 2.2rem;
 font-weight: 600;
+  @media (min-width: 600px) {
+    font-size: 2.5rem;
+      }
+  @media (min-width: 800px) {
+    font-size: 3rem;
+      }
 `;
 
 export const HeaderBlue = Styled.h2`
 color:#4066b0;
 font-size: 2.1rem;
 font-weight: 600;
+  @media (min-width: 800px) {
+    font-size: 3rem;
+      }
 `;
 
 export const HeaderBlue2 = Styled.h2`
 color:#4066b0;
 font-size: 1.8rem;
 font-weight: 600;
+  @media (min-width: 800px) {
+    font-size: 2.8rem;
+      }
+
 `;
 
 export const Header3 = Styled.h3`
 color:black;
 font-size: 1.8rem;
 font-weight: 600;
+    @media (min-width: 800px) {
+    font-size: 2.5rem;
+      }
 `;
 
 export const Para = Styled.p`
@@ -133,6 +191,12 @@ color:white;
 text-align: justify;
 text-justify: inter-word;
 font-size: 1.5rem;
+  @media (min-width: 600px) {
+    font-size: 1.6rem;
+      }
+  @media (min-width: 800px) {
+    font-size: 2.3rem;
+      }
 `;
 
 export const ParaPurp = Styled.p`
@@ -147,16 +211,25 @@ color:#4066b0;
 text-align: justify;
 text-justify: inter-word;
 font-size: 1.5rem;
+@media (min-width: 800px) {
+    font-size: 2.3rem;
+      }
 `;
 
 export const ParaBlack = Styled.p`
 color:black;
 font-size: 1.5rem;
+  @media (min-width: 800px) {
+    font-size: 2.3rem;
+      }
 `;
 
 export const Captions = Styled.p`
 color:black;
 font-size: 1rem;
+@media (min-width: 800px) {
+    font-size: 1.4rem;
+      }
 `;
 
 //Buttons
@@ -172,6 +245,24 @@ font-size: 1.5rem;
 font-weight:600;
 padding: 3%;
 width: 30%;
+  @media (min-width: 600px) {
+    border-radius: 60px;
+      }
+  @media (min-width: 800px) {
+    font-size: 2rem;
+      }
+  @media (min-device-width: 1600px) 
+  and (max-device-width: 2000px) {
+    width:20%;
+    padding: 1%;
+    font-size: 1.3rem;
+  }
+    @media (min-device-width: 2500px) 
+  and (max-device-width: 3000px) {
+    width: 10%;
+    padding: 0.5%;
+    font-size: 1.2rem;
+  }
 `;
 
 //Images
@@ -181,6 +272,14 @@ export const MainLogo = Styled.img.attrs({
 })`
 display:flex;
 width: 40%;
+ @media (min-device-width: 1600px) 
+  and (max-device-width: 2000px) {
+    width:30%;
+  }
+    @media (min-device-width: 2500px) 
+  and (max-device-width: 3000px) {
+    width: 20%;
+  }
 `;
 
 export const PiggyBank = Styled.img.attrs({
@@ -190,6 +289,14 @@ display:flex;
 width: 40%;
 margin: 5% 0;
 animation: ${Wiggle} 0.9s both;
+ @media (min-device-width: 1600px) 
+  and (max-device-width: 2000px) {
+    width:25%;
+  }
+    @media (min-device-width: 2500px) 
+  and (max-device-width: 3000px) {
+    width: 25%;
+  }
 `;
 
 export const LinkImg = Styled.img.attrs({
@@ -198,6 +305,9 @@ export const LinkImg = Styled.img.attrs({
 display:flex;
 margin-right: 5%;
 width: 40%;
+  @media (min-width: 1000px) {
+    width:30%;
+      }
 `;
 
 export const EarnImg = Styled.img.attrs({
@@ -206,6 +316,9 @@ export const EarnImg = Styled.img.attrs({
 display:flex;
 margin-left: 6%;
 width: 40%;
+  @media (min-width: 1000px) {
+    width:30%;
+      }
 `;
 
 export const RewardImg = Styled.img.attrs({
@@ -214,13 +327,19 @@ export const RewardImg = Styled.img.attrs({
 display:flex;
 margin-right: 5%;
 width: 40%;
+  @media (min-width: 1000px) {
+    width:30%;
+      }
 `;
 
 export const BrokeImg = Styled.img.attrs({
   src: Poor,
 })`
 display:flex;
-width:40%
+width:40%;
+  @media (min-width: 1000px) {
+    width:30%;
+      }
 `;
 
 export const Testimonial1 = Styled.img.attrs({
@@ -231,6 +350,10 @@ margin: 5% 0;
 width: 125px;
 border-radius: 75px;
 box-shadow: 5px 5px 5px 0px rgba(173,173,173,1);
+  @media (min-width: 600px) {
+    width: 180px;
+    border-radius: 50%;
+      }
 `;
 
 export const Testimonial2 = Styled.img.attrs({
@@ -241,6 +364,10 @@ margin: 5% 0;
 width: 125px;
 border-radius: 75px;
 box-shadow: 5px 5px 5px 0px rgba(173,173,173,1);
+  @media (min-width: 600px) {
+    width: 180px;
+    border-radius: 50%;
+      }
 `;
 
 export const Testimonial3 = Styled.img.attrs({
@@ -252,6 +379,11 @@ height: 120px;
 width: auto;
 border-radius: 1540px;
 box-shadow: 5px 5px 5px 0px rgba(173,173,173,1);
+  @media (min-width: 600px) {
+    width: 180px;
+    height: auto;
+    border-radius: 50%;
+      }
 `;
 
 //other
@@ -264,4 +396,7 @@ width:100%;
 export const TitleDivider2 = Styled.hr`
 color: #708090;
 width:80%;
+  @media (min-width: 600px) {
+    width: 100%;
+      }
 `;
