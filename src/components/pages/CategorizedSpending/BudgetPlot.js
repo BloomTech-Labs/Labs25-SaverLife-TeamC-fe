fe26c52cc49cb71788764275cbc08b678dc76866
Victoria  Topham <topham.victoria@gmail.com>
2020-08-31 13:17:15 -0700

    Implemented dual bar chart

--- src/components/pages/CategorizedSpending/BudgetPlot.js
+++ src/components/pages/CategorizedSpending/BudgetPlot.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {
+    state: state,
+  };
+};
 const BudgetPlot = props => {
   const { data, categories } = props;
+
+  const { state } = props;
+  const budgetObject = state.budgetReducer;
+  const budgetItems = [];
+  categories.forEach(cat => {
+    budgetItems.push(budgetObject[cat]);
+  });
+
+  console.log({ budgetItems });
+
   return (
     <>
       <Plot
@@ -11,8 +28,17 @@ const BudgetPlot = props => {
             x: data,
             y: categories,
             type: 'bar',
-            width: 0.6,
+            width: 0.3,
             orientation: 'h',
+            name: 'Your Spending',
+          },
+          {
+            x: budgetItems,
+            y: categories,
+            type: 'bar',
+            width: 0.3,
+            orientation: 'h',
+            name: 'Your Budget',
           },
         ]}
         layout={{
@@ -31,4 +57,4 @@ const BudgetPlot = props => {
   );
 };
 
-export default BudgetPlot;
+export default connect(mapStateToProps, {})(BudgetPlot);
