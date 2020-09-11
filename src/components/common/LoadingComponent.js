ac4f09176618c19ba54c246c29a99606816ee633
Christina Harris <christinathedesigner@outlook.com>
2020-09-11 07:00:20 -0700

    loader added to home page and to transactions page

--- src/components/common/LoadingComponent.js
+++ src/components/common/LoadingComponent.js
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Loader from 'react-loader-spinner';
 
-function LoadingComponent(props) {
-  const { message } = props;
-
-  return <div>{message}</div>;
+function LoadingComponent() {
+  return (
+    <div>
+      <Loader
+        type="Watch"
+        color="#c01089"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+    </div>
+  );
 }
 
 export default LoadingComponent;
