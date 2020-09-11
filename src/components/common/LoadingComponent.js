304a54c936e82dbdb911587d00e2491bd32bde76
Christina Harris <christinathedesigner@outlook.com>
2020-09-11 11:57:22 -0700

    styled loader

--- src/components/common/LoadingComponent.js
+++ src/components/common/LoadingComponent.js
@@ -1,18 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Loader from 'react-loader-spinner';
+import { MainWrapper, LoaderWrapper, Para } from './loadingComponentStyle';
 
 function LoadingComponent() {
   return (
-    <div>
-      <Loader
-        type="Watch"
-        color="#c01089"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
-    </div>
+    <MainWrapper>
+      <LoaderWrapper>
+        <Loader
+          type="Watch"
+          color="#c01089"
+          height={100}
+          width={100}
+          timeout={12000} //3 secs
+        />
+      </LoaderWrapper>
+      <Para>SaverLife Is Retrieving Your Information</Para>
+    </MainWrapper>
   );
 }
 
--- src/components/common/loadingComponentStyle.js
+++ src/components/common/loadingComponentStyle.js
@@ -1,1 +1,28 @@
 import Styled from 'styled-components';
+import { Bounce } from './keyframes';
+
+export const MainWrapper = Styled.div`
+display:flex;
+width:100%;
+height:100vh;
+justify-content: center;
+align-items: center;
+align-content: center;
+margin: 0 auto;
+flex-direction: column;
+
+`;
+
+export const LoaderWrapper = Styled.div`
+animation: ${Bounce} 0.5s both;
+display:flex;
+justify-content: center;
+align-items: center;
+align-content: center;
+`;
+
+export const Para = Styled.p`
+color: #c01089;
+padding:3%;
+font-size: 1.3rem;
+`;
