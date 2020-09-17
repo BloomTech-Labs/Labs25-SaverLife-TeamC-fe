654697410fceccae869c9ba555fc5deb0954ab8b
Victoria  Topham <topham.victoria@gmail.com>
2020-09-17 12:07:28 -0700

    Move logout to nav bar for desktop, can log out successfully

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -77,6 +77,7 @@ const mapStateToProps = state => {
 //TODO: move these cards into separate components probably
 function RenderHomePage(props) {
   const { userInfo, authService, state } = props;
+  console.log({ props });
   const transactions = state.transactionReducer.data;
   // get 5 most recent transactions for the table
   //THIS IS FOR TRANSACTIONS
