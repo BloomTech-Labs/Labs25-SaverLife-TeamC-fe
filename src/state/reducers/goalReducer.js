4d6611d05e809dc932f0afd8dfaa03ed728ce2f2
unknown <locbgiang@gmail.com>
2020-09-22 16:16:29 -0700

    chore: connected to the back-end for goal data

--- src/state/reducers/goalReducer.js
+++ src/state/reducers/goalReducer.js
@@ -1,22 +1,23 @@
 const initialState = {
-  totalSaved: 0,
-  totalSavedDate: 0,
   goal: 0,
+  endYear: 0,
+  endMonth: 0,
+  monthLeft: 0,
+  nextMonthForcast: 0,
+  suggestedBudget: 0,
 };
 
 export const goalReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_GOAL_PROGRESS_SUCCESS':
+    case 'FETCH_BUDGET_SUCCESS':
       return {
         ...state,
-        totalSaved: action.payload,
-      };
-    case 'FETCH_PROFILE_SUCCESS':
-      return {
-        ...state,
-        totalSaved: action.payload,
+        goal: action.payload.goalAmount,
+        endYear: action.payload.goalEndYear,
+        endMonth: action.payload.goalEndMonth,
+        monthLeft: action.payload.monthLeft,
+        nextMonthForcast: action.payload.next,
       };
   }
-
   return state;
 };
