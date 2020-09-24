f83151bbfb72cda91edcf6bcba894494c04b46a5
unknown <locbgiang@gmail.com>
2020-09-24 09:13:19 -0700

    finalizing code, goal connected to the back-end

--- src/state/actions/profileActions.js
+++ src/state/actions/profileActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+//import {axiosWithAuth} from '../../utils/axiosWithAuth';
 
 export const fetchProfile = () => {
   let userId = JSON.parse(window.localStorage.getItem('okta-token-storage'));
@@ -9,6 +10,38 @@ export const fetchProfile = () => {
       .then(res => {
         console.log(res.data);
         dispatch({ type: 'FETCH_PROFILE_SUCCESS', payload: res.data });
+      });
+  };
+};
+
+export const inputGoal = inputData => {
+  let userId = JSON.parse(window.localStorage.getItem('okta-token-storage'));
+  userId = userId.idToken.claims.sub;
+  let inputBody = {
+    id: userId,
+    goalAmount: inputData,
+  };
+  return dispatch => {
+    axios
+      .put('https://saver-life-team-c.herokuapp.com/profile/', inputBody)
+      .then(res => {
+        dispatch({ type: 'SET_GOAL_SUCCESS', payload: res.data.profile });
+      });
+  };
+};
+
+export const fetchBudget = () => {
+  let userId = JSON.parse(window.localStorage.getItem('okta-token-storage'));
+  userId = userId.idToken.claims.sub;
+  console.log('fetching budget');
+  return dispatch => {
+    axios
+      .get(
+        `https://saver-life-team-c.herokuapp.com/profile/fetching/budget/${userId}`
+      )
+      .then(res => {
+        console.log('after calling ds', res);
+        dispatch({ type: 'FETCH_BUDGET_SUCCESS', payload: res.data });
       });
   };
 };
